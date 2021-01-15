8337c85f2c97a71e54277d2656aa45d621234f49
schecbr <schecbr@gmail.com>
2021-01-15 09:50:21 -0500

    added encrypt functionality

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -16,7 +16,16 @@ limitations under the License.
 package cmd
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -24,42 +33,52 @@ import (
 var encryptCmd = &cobra.Command{
 	Use:   "encrypt",
 	Short: "The encrypt command will encrypt a file",
-	Long: "Krypt is CLI that allows users to easily encrypt and decrypt files with sane defaults.",
+	Long: `Krypt is a CLI that allows users to easily encrypt and decrypt files
+	with sane defaults.  You can encrypt a file by simply calling
+	encrypt and passing in a passphrase to hash.`,
 
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("encrypt called")
-		encryptFile("master.key", []byte("Hello World"), "password1")
+		passphrase, _ := cmd.Flags().GetString("passphrase")
+
+		if passphrase == "" {
+			return errors.New("Must specify a passphrase")
+		}
+
+		fmt.Println("passphrase: " + passphrase)
+		encryptFile("master.key", []byte("Hello World"), passphrase)
+		return errors.New("Something went wrong")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(encryptCmd)
+	encryptCmd.Flags().StringP("passphrase", "s", "", "Passphrase to create hash")
 
 }
 
 func createHash(key string) string {
-        hasher := md5.New()
-        hasher.Write([]byte(key))
-        return hex.EncodeToString(hasher.Sum(nil))
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func encrypt(data []byte, passphrase string) []byte {
-        block, _ := aes.NewCipher([]byte(createHash(passphrase)))
-        gcm, err := cipher.NewGCM(block)
-        if err != nil {
-                panic(err.Error())
-        }
-        nonce := make([]byte, gcm.NonceSize())
-        if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-                panic(err.Error())
-        }
-        ciphertext := gcm.Seal(nonce, nonce, data, nil)
-        return ciphertext
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
 }
 
 func encryptFile(filename string, data []byte, passphrase string) {
-        f, _ := os.Create(filename)
-        defer f.Close()
-        f.Write(encrypt(data, passphrase))
+	f, _ := os.Create(filename)
+	defer f.Close()
+	f.Write(encrypt(data, passphrase))
 }
-
